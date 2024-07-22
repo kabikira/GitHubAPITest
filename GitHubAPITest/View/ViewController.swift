cde26730da931daeddd10d8a0c29e4ce0d601102
kabikira <kabikirar2@protonmail.com>
2024-07-22 09:04:54 +0900

    add

--- GitHubAPITest/View/ViewController.swift
+++ GitHubAPITest/View/ViewController.swift
@@ -21,38 +21,53 @@ class ViewController: UIViewController {
     }
 
     let repositoryManager = GitHubRepositoryManager()
-    var gitHubRepository: [GitHubRepository] = []
+    var gitHubRepositories: [GitHubRepository] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // searchButtonにアクションを追加
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func searchButtonTapped() {
+        guard let username = searchTextField.text, !username.isEmpty else {
+            print("Username is empty")
+            return
+        }
+
+        // 検索中にインジケータを表示
+        indicator.startAnimating()
+
         Task {
             do {
-                try await repositoryManager.load(user: "kabikira")
-                if let repos = repositoryManager.repos {
-                    self.gitHubRepository = repos
+                try await repositoryManager.load(user: username)
+                if let repos = self.repositoryManager.repos {
+                    self.gitHubRepositories = repos
                     self.tableView.reloadData()
                 }
             } catch {
                 print("Failed to load repositories: \(error)")
             }
+            // 検索完了後にインジケータを非表示
+            self.indicator.stopAnimating()
         }
     }
-    
+
 }
 
 extension ViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gitHubRepository.count
+        return gitHubRepositories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: GitHubTableViewCell.className) as? GitHubTableViewCell else {
             fatalError()
         }
-        let gitHubRepository = gitHubRepository[indexPath.row]
-        cell.configure(gitHubRepository: gitHubRepository)
+        let gitHubRepositories = gitHubRepositories[indexPath.row]
+        cell.configure(gitHubRepository: gitHubRepositories)
         return cell
     }
 }
